87ce099bbbd56247b79e6eea14764f38004dcee1
Reon Fernandes <reondias@gmail.com>
2024-02-11 19:47:20 +0530

    App.java is updated

--- App.java
+++ App.java
@@ -1,11 +1,35 @@
-public class App{
-    public static void main(String[] args) throws Exception{
-        // Students students = new Students("", 0, "", 0);
-        // StudentsDAO.insertData(students);
+import java.util.Scanner;
 
-        //StudentsDAO.printData();
-        //StudentsDAO.updateData();
+public class App {
+    public static void main(String[] args) throws Exception {
+        Students students = new Students("", 0, "", 0);
 
-        StudentsDAO.deleteData();
+        System.out.println("Welcome to Student Management System.");
+        System.out.print("Select Operation (insert, read, update, delete): ");
+        
+        try (Scanner scanner = new Scanner(System.in)) {
+            try{
+                String choice = scanner.nextLine().toLowerCase();
+                switch (choice) {
+                    case "insert":
+                        StudentsDAO.insertData(students);
+                        break;
+                    case "read":
+                        StudentsDAO.printData();
+                        break;
+                    case "update":
+                        StudentsDAO.updateData();
+                        break;
+                    default:
+                        StudentsDAO.deleteData();
+                        break;
+                }
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
